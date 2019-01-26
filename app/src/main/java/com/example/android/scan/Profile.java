29391d1c1d31b6326107a98fe38ad706de621ab4
ns1017 <ns1017@ic.ac.uk>
2019-01-26 16:28:21 +0000

    did a ton of file-handling stuff

--- app/src/main/java/com/example/android/scan/Profile.java
+++ app/src/main/java/com/example/android/scan/Profile.java
@@ -1,12 +1,16 @@
 package com.example.android.scan;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -14,22 +18,27 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-import static java.lang.System.in;
 
 public class Profile extends AppCompatActivity {
 
     public static final String PROFILE_FILENAME = "profile.txt";
 
+    private File profile;
+
     private TextView mTextMessage;
     private EditText mNameInput;
     private AutoCompleteTextView mInterestInput;
     private Button mAddButton;
+    private Button mNameSetButton;
 
     private List<String> interestList;
     private String[] hobbies;
@@ -55,9 +64,9 @@ public class Profile extends AppCompatActivity {
     };
 
     private void loadProfile() throws IOException {
-        final File profile = new File(this.getApplicationContext().getFilesDir(), PROFILE_FILENAME);
         if (!profile.createNewFile()) {
-            Scanner br = new Scanner(new InputStreamReader(in));
+            FileInputStream fstream = new FileInputStream(PROFILE_FILENAME);
+            Scanner br = new Scanner(new InputStreamReader(fstream));
             if (br.hasNext()) {
                 String name = br.nextLine();
                 mNameInput.setText(name);
@@ -67,42 +76,99 @@ public class Profile extends AppCompatActivity {
                 if (!TextUtils.isDigitsOnly(interestKey)) {
                     Log.d("ERR", "shouldnt get here");
                 } else {
-                    addToInterestList(hobbies[Integer.parseInt(interestKey)]);
+                    addToHobbyList(Integer.parseInt(interestKey));
                 }
             }
+            fstream.close();
             return;
         }
     }
 
-    private void addToInterestList(String hobby) {
-        interestList.add(hobby);
-        //todo: add to ui
+    private void updateHobbyListDisplay() {
+        //todo: update the displayed ui element that has all the hobbies
+    }
+
+    private void addToHobbyList(int i) {
+        //todo: add to file
+        //todo: update hobby list ui
+    }
+
+    private void removeFromHobbyList(int i) {
+        // todo: remove from file
+        // todo: updated hobby list ui
+    }
+
+    private void updateName(String newName) throws IOException {
+        final PrintWriter writer = new PrintWriter(profile);
+
+        if (profile.createNewFile()) {
+            writer.print(newName);
+            writer.close();
+            return;
+        }
+
+        List<String> fileLines = new ArrayList<>();
+        FileInputStream fstream = new FileInputStream(PROFILE_FILENAME);
+        Scanner br = new Scanner(new InputStreamReader(fstream));
+        while (br.hasNext()) {
+            String line = br.nextLine();
+            if (!TextUtils.isDigitsOnly(line)) {
+                Log.d("ERR", "shouldnt get here");
+            } else {
+                fileLines.add(line);
+            }
+        }
+
+        fileLines.set(0, newName);
+        for (String fl : fileLines) {
+            writer.println(fl);
+        }
+        writer.close();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Context context = this.getApplicationContext();
         setContentView(R.layout.activity_profile);
+        profile = new File(context.getFilesDir(), PROFILE_FILENAME);
 
         hobbies = (String[]) getResources().getStringArray(R.array.hobby_array);
 
-        mAddButton = (Button) findViewById(R.id.button); //listener must call addinterest
+        mAddButton = (Button) findViewById(R.id.button_add);
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addToHobbyList(mInterestInput.getListSelection());
+            }
+        });
 
         mInterestInput = (AutoCompleteTextView) findViewById(R.id.at_interest_input);
+        // TODO: only allow autocomplete hobby values
         ArrayAdapter<String> mInterestInputAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_dropdown_item_1line, Arrays.asList(hobbies));
         mInterestInput.setAdapter(mInterestInputAdapter);
 
-        mNameInput = (EditText) findViewById(R.id.et_name); //listener must edit text in first line
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mNameInput = (EditText) findViewById(R.id.et_name);
+        mNameSetButton = (Button) findViewById(R.id.button_set);
+        mNameSetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    updateName(mNameInput.getText().toString());
+                } catch (IOException e) {
+                    Log.d("ERR", "shouldnt get here 3");
+                }
+            }
+        });
 
         /*try {
             loadProfile();
         } catch (IOException e) {
             Log.d("ERR", "shouldnt get here 2");
         }*/
-    }
 
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
 }
