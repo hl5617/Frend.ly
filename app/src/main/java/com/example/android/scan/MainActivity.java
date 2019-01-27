0390a835731841f016cc48234aa427c23ccbd7e4
ns1017 <ns1017@ic.ac.uk>
2019-01-27 01:43:37 +0000

    implemented hobby comparison against profile.txt

--- app/src/main/java/com/example/android/scan/MainActivity.java
+++ app/src/main/java/com/example/android/scan/MainActivity.java
@@ -6,13 +6,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import static com.example.android.scan.Profile.PROFILE_FILENAME;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +47,40 @@ public class MainActivity extends AppCompatActivity {
     private List<String> listDataHeader;
     private HashMap<String, List<String>> listDataChild;
 
+    private List<String> hobbies;
+    private String name;
+    private File profile;
+    private String[] possibleHobbies;
+
+    private void readProfile() throws IOException {
+        //mTestView.setText("readProfile");
+        hobbies = new ArrayList<>();
+        FileInputStream is;
+        BufferedReader reader;
+        if (profile.exists()) {
+            is = new FileInputStream(profile);
+            reader = new BufferedReader(new InputStreamReader(is));
+            String line = reader.readLine();
+            if (line != null) {
+                name = line;
+            }
+            while(line != null){
+                line = reader.readLine();
+                if (line != null) {
+                    hobbies.add(possibleHobbies[Integer.parseInt(line)]);
+                }
+            }
+        }
+    }
+
+    private void giveNoName() throws IOException {
+        name = "name not set";
+        final PrintWriter writer = new PrintWriter(profile);
+
+        writer.println(name);
+        writer.close();
+    }
+
     private void prepareListData() {
         listDataHeader = new ArrayList<String>();
         listDataChild = new HashMap<String, List<String>>();
@@ -56,15 +99,42 @@ public class MainActivity extends AppCompatActivity {
         addDude("steve", steveHobbies);
     }
 
-    private void addDude(String name, List<String> hobbies) {
+    private void addDude(String name, List<String> theirHobbies) {
         listDataHeader.add(name);
-        listDataChild.put(listDataHeader.get(listDataHeader.size() - 1), hobbies);
+        List<String> theirHobbiesFiltered = new ArrayList<>();
+        for (String h : theirHobbies) {
+            if (hobbies.contains(h)) {
+                theirHobbiesFiltered.add(h);
+            }
+        }
+        listDataChild.put(listDataHeader.get(listDataHeader.size() - 1), theirHobbiesFiltered);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        possibleHobbies = (String[]) getResources().getStringArray(R.array.hobby_array);
+
+        profile = new File(this.getApplicationContext().getFilesDir(), PROFILE_FILENAME);
+
+        if (profile.exists()) {
+            try {
+                readProfile();
+            } catch (IOException e){
+                Log.d("ERR", "IOEXc");
+            }
+        } else {
+            try {
+                profile.createNewFile();
+                hobbies = new ArrayList<>();
+                giveNoName();
+            } catch (IOException e) {
+                Log.d("ERR", "IOEXc");
+            }
+        }
+
 
         expListView = (ExpandableListView) findViewById(R.id.lvExp);
 
