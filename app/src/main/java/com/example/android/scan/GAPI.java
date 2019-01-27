ce769b8aaac33914ff984bea4e363cc1d7bbf409
Samuel Trew <samuel.trew@outlook.com>
2019-01-27 05:39:09 +0000

    Code is nearly correct in theory but app never opens/just crashes

--- app/src/main/java/com/example/android/scan/GAPI.java
+++ app/src/main/java/com/example/android/scan/GAPI.java
@@ -4,14 +4,18 @@ import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,6 +38,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -42,258 +47,10 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 
 public abstract class GAPI extends AppCompatActivity {
-
-    private static final String TAG = "FRENDS";
-
-    public String endpointId;
-
-    private TextView statusText;
-
-    Payload payload;
-
-    Context context = getApplicationContext();
-
-    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
-
-    /** Our handler to Nearby Connections. */
-    private ConnectionsClient connectionsClient = Nearby.getConnectionsClient(context);
-
-    /** The devices we've discovered near us. */
-//    private final Map<String, ConnectionsActivity.Endpoint> mDiscoveredEndpoints = new HashMap<>();
-
-
-//    private final Map<String, ConnectionsActivity.Endpoint> mPendingConnections = new HashMap<>();
-
-    /**
-     * The devices we are currently connected to. For advertisers, this may be large. For discoverers,
-     * there will only be one entry in this map.
-     */
-//    private final Map<String, ConnectionsActivity.Endpoint> mEstablishedConnections = new HashMap<>();
-
-    /**
-     * True if we are asking a discovered device to connect to us. While we ask, we cannot ask another
-     * device.
-     */
-    private boolean mIsConnecting = false;
-
-    /** True if we are discovering. */
-    private boolean mIsDiscovering = false;
-
-    /** True if we are advertising. */
-    private boolean mIsAdvertising = false;
-
-
-    private String getUserNickname() {
-        return "same";
-    }
-
-
-    private static final String[] REQUIRED_PERMISSIONS =
-            new String[] {
-                    Manifest.permission.BLUETOOTH,
-                    Manifest.permission.BLUETOOTH_ADMIN,
-                    Manifest.permission.ACCESS_WIFI_STATE,
-                    Manifest.permission.CHANGE_WIFI_STATE,
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-            };
-
-    public Payload getPayload() {
-        return payload;
-    }
-
-    public void setPayload(Payload load) {
-        payload = load;
-    }
-
-
-
-    private final PayloadCallback payloadCallback =
-            new PayloadCallback() {
-                @Override
-                public void onPayloadReceived(String endpointId, Payload payload) {
-                    // A new payload is being sent over.
-                    setPayload(payload);
-                    Toast.makeText(context, 0, Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
-                    // Payload progress has updated.
-
-                }
-            };
-
-    public void setEndpointId(String id) {
-        endpointId = id;
-    }
-
-    public String getEndpointId() {
-        return endpointId;
-    }
-
-    private void setStatusText(String text) {
-        statusText.setText(text);
-    }
-
-    private final ConnectionLifecycleCallback connectionLifecycleCallback =
-            new ConnectionLifecycleCallback() {
-                @Override
-                public void onConnectionInitiated(String endpointId, ConnectionInfo connectionInfo) {
-                    // Automatically accept the connection on both sides.
-                    connectionsClient.acceptConnection(endpointId, payloadCallback);
-                }
-
-                @Override
-                public void onConnectionResult(String endpointId, ConnectionResolution result) {
-                    switch (result.getStatus().getStatusCode()) {
-                        case ConnectionsStatusCodes.STATUS_OK:
-                            // We're connected! Can now start sending and receiving data.
-                            Log.i(TAG, "onConnectionResult: connection successful");
-
-                            connectionsClient.stopDiscovery();
-                            connectionsClient.stopAdvertising();
-
-                            setEndpointId(endpointId);
-                            setStatusText(getString(0));
-                        case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
-                            // The connection was rejected by one or both sides.
-                            break;
-                        default:
-                            // The connection was broken before it was accepted.
-                            break;
-                    }
-                }
-
-                @Override
-                public void onDisconnected(String endpointId) {
-                    // We've been disconnected from this endpoint. No more data can be
-                    // sent or received.
-                }
-            };
-
-    private final EndpointDiscoveryCallback endpointDiscoveryCallback =
-            new EndpointDiscoveryCallback() {
-                @Override
-                public void onEndpointFound(String endpointId, DiscoveredEndpointInfo info) {
-                    // An endpoint was found. We request a connection to it.
-                    connectionsClient
-                            .requestConnection(getUserNickname(), endpointId, connectionLifecycleCallback)
-                            .addOnSuccessListener(
-                                    (Void unused) -> {
-                                        // We successfully requested a connection. Now both sides
-                                        // must accept before the connection is established.
-                                    })
-                            .addOnFailureListener(
-                                    (Exception e) -> {
-                                        // Nearby Connections failed to request the connection.
-                                    });
-                }
-
-                @Override
-                public void onEndpointLost(String endpointId) {
-                    // A previously discovered endpoint has gone away.
-                }
-            };
-
-
-    private void startDiscovery() {
-        DiscoveryOptions discoveryOptions =
-                new DiscoveryOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build();
-        connectionsClient
-                .startDiscovery("com.example.android.scan", endpointDiscoveryCallback, discoveryOptions)
-                .addOnSuccessListener(
-                        (Void unused) -> {
-                            // We're discovering!
-                        })
-                .addOnFailureListener(
-                        (Exception e) -> {
-                            // We're unable to start discovering.
-                        });
-    }
-
-    private void startAdvertising() {
-        AdvertisingOptions advertisingOptions =
-                new AdvertisingOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build();
-        connectionsClient
-                .startAdvertising(
-                        getUserNickname(), "com.example.android.scan", connectionLifecycleCallback, advertisingOptions)
-                .addOnSuccessListener(
-                        (Void unused) -> {
-                            // We're advertising!
-                        })
-                .addOnFailureListener(
-                        (Exception e) -> {
-                            // We were unable to start advertising.
-                        });
-    }
-
-    /** Stops advertising. */
-    protected void stopAdvertising() {
-        mIsAdvertising = false;
-        connectionsClient.stopAdvertising();
-    }
-
-    /** Returns {@code true} if currently advertising. */
-    protected boolean isAdvertising() {
-        return mIsAdvertising;
-    }
-
-//    @Override
-    public void onConnectionInitiated(String endpointId, ConnectionInfo info) {
-        new AlertDialog.Builder(context)
-                .setTitle("Accept connection to " + info.getEndpointName())
-                .setMessage("Confirm the code matches on both devices: " + info.getAuthenticationToken())
-                .setPositiveButton(
-                        "Accept",
-                        (DialogInterface dialog, int which) ->
-                                // The user confirmed, so we can accept the connection.
-                                connectionsClient.acceptConnection(endpointId, payloadCallback))
-                .setNegativeButton(
-                        android.R.string.cancel,
-                        (DialogInterface dialog, int which) ->
-                                // The user canceled, so we should reject the connection.
-                                Nearby.getConnectionsClient(context).rejectConnection(endpointId))
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .show();
-    }
-
-
-    private void getConnection(String endpointId) {
-            connectionsClient.requestConnection(endpointId, endpointId,connectionLifecycleCallback)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    public void onSuccess(Void aVoid) {
-                        Log.d("ERR", "Requesting connection..");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e("ERR", "Error requesting connection", e);
-                    }
-                });
-    }
-
-    public void transfer() {
-        connectionsClient.sendPayload(endpointId, payload);
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- app/src/main/java/com/example/android/scan/MainActivity.java
+++ app/src/main/java/com/example/android/scan/MainActivity.java
@@ -1,21 +1,41 @@
 package com.example.android.scan;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
 import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.connection.Strategy;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
@@ -25,10 +45,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    Context context = getApplicationContext();
-
-    ConnectionsClient connectionsClient = Nearby.getConnectionsClient(context);
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -47,42 +63,109 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    Payload payload_g;
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+    private static final String TAG = "FREND.LY";
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
+    private static final String[] REQUIRED_PERMISSIONS =
+            new String[]{
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_WIFI_STATE,
+                    Manifest.permission.CHANGE_WIFI_STATE,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+            };
 
-    @Override
-    protected void onStop() {
-        Nearby.getConnectionsClient(context).stopAllEndpoints();
+    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
 
-        super.onStop();
-    }
 
-    @CallSuper
-    @Override
-    public void onRequestPermissionsResult(
-            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        for (int grantResult : grantResults) {
-            if (grantResult == PackageManager.PERMISSION_DENIED) {
-                Toast.makeText(this, 0, Toast.LENGTH_LONG).show();
-                finish();
-                return;
-            }
-        }
-        recreate();
-    }
+    //Our Strategy
+    private static final Strategy STRATEGY = Strategy.P2P_STAR;
+
+    //Our handle to Nearby Clients
+    private ConnectionsClient connectionsClient;
+
+    //Our name
+    private final String name = "NRSHF";
+
+    private String otherID;
+    private String otherName;
+
+    private Button disconnectButton;
+    private Button connectButton;
+
+    private TextView statusText;
+    private TextView otherText;
+
+    // Callbacks for receiving payloads
+    private final PayloadCallback payloadCallback =
+            new PayloadCallback() {
+                @Override
+                public void onPayloadReceived(String endpointId, Payload payload) {
+                    payload_g = payload;
+                }
+
+                @Override
+                public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
+                }
+            };
+
+    // Callbacks for finding other devices
+    private final EndpointDiscoveryCallback endpointDiscoveryCallback =
+            new EndpointDiscoveryCallback() {
+                @Override
+                public void onEndpointFound(String endpointId, DiscoveredEndpointInfo info) {
+                    Log.i(TAG, "onEndpointFound: endpoint found, connecting");
+                    connectionsClient.requestConnection(name, endpointId, connectionLifecycleCallback);
+                }
+
+                @Override
+                public void onEndpointLost(String endpointId) {}
+            };
+
+    // Callbacks for connections to other devices
+    private final ConnectionLifecycleCallback connectionLifecycleCallback =
+            new ConnectionLifecycleCallback() {
+                @Override
+                public void onConnectionInitiated(String endpointId, ConnectionInfo connectionInfo) {
+                    Log.i(TAG, "onConnectionInitiated: accepting connection");
+                    connectionsClient.acceptConnection(endpointId, payloadCallback);
+                    otherName = connectionInfo.getEndpointName();
+                }
+
+                @Override
+                public void onConnectionResult(String endpointId, ConnectionResolution result) {
+                    if (result.getStatus().isSuccess()) {
+                        Log.i(TAG, "onConnectionResult: connection successful");
+
+                        connectionsClient.stopDiscovery();
+                        connectionsClient.stopAdvertising();
+
+                        otherID = endpointId;
+                        setOpponentName(otherName);
+                        //setStatusText(getString(R.string.status_connected));
+                        setButtonState(true);
+                    } else {
+                        Log.i(TAG, "onConnectionResult: connection failed");
+                    }
+                }
+
+                @Override
+                public void onDisconnected(String endpointId) {
+                    Log.i(TAG, "onDisconnected: disconnected from the opponent");
+                }
+            };
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//    }
+
 
     public void gotoProfile() {
         Context context = MainActivity.this;
@@ -91,35 +174,130 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void disconnect(View view) {
-        connectionsClient.disconnectFromEndpoint("");
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        disconnectButton = findViewById(R.id.disconnect);
+        connectButton = findViewById(R.id.connect);
+
+        statusText = findViewById(R.id.status);
+
+        TextView nameView = findViewById(R.id.name);
+        //nameView.setText(getString(R.string.codename, name));
+
+        connectionsClient = Nearby.getConnectionsClient(this);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!hasPermissions(this, REQUIRED_PERMISSIONS)) {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        connectionsClient.stopAllEndpoints();
+        super.onStop();
+    }
+
+    /** Returns true if the app was granted all the permissions. Otherwise, returns false. */
+    private static boolean hasPermissions(Context context, String... permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /** Handles user acceptance (or denial) of our permission request. */
+    @CallSuper
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode != REQUEST_CODE_REQUIRED_PERMISSIONS) {
+            return;
+        }
+
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                //Toast.makeText(this, R.string.error_missing_permissions, Toast.LENGTH_LONG).show();
+                finish();
+                return;
+            }
+        }
+        recreate();
     }
 
 
 
-//
-//    public List<String> checkHosts(String subnet) throws IOException {
-//
-//        InetAddress localHost = Inet4Address.getLocalHost();
-//        NetworkInterface networkInterface = NetworkInterface.getByInetAddress(localHost);
-//        short temp = networkInterface.getInterfaceAddresses().get(0).getNetworkPrefixLength();
-//        System.out.println(temp);
-//
-//
-//
-//        List<String> results = new ArrayList<>();
-//
-//        int timeout = 1000;
-//
-//        for (int i = 1; i < 255; i++) {
-//            String host = subnet + "." + i;
-//            if (InetAddress.getByName(host).isReachable(timeout)) {
-//                results.add(host);
-//            }
-//
-//        }
-//        return results;
-//
-//    }
+    /** Finds an Frend using Nearby Connections. */
+    public void findFrend(View view) {
+        startAdvertising();
+        startDiscovery();
+        //setStatusText(getString(R.string.status_searching));
+    }
+
+    public void disconnect(View view) {
+        connectionsClient.disconnectFromEndpoint(otherID);
+    }
+
+
+    /** Starts looking for Frends using Nearby Connections. */
+    private void startDiscovery() {
+        // Note: Discovery may fail.
+        connectionsClient.startDiscovery(
+                getPackageName(), endpointDiscoveryCallback,
+                new DiscoveryOptions.Builder().setStrategy(STRATEGY).build());
+    }
+
+    /** Broadcasts our presence using Nearby Connections so other players can find us. */
+    private void startAdvertising() {
+        // Note: Advertising may fail.
+        connectionsClient.startAdvertising(
+                name, getPackageName(), connectionLifecycleCallback,
+                new AdvertisingOptions.Builder().setStrategy(STRATEGY).build());
+    }
+
+
+    /** Sends the user's file to the Frend. */
+    private void sendFile(File file) throws FileNotFoundException {
+
+        connectionsClient.sendPayload(
+                otherID, Payload.fromFile(file));
+
+        //setStatusText(getString(R.string.game_choice, choice.name()));
+        connectButton.setEnabled(false);
+    }
+
+    /** Shows a status message to the user. */
+    private void setStatusText(String text) {
+        statusText.setText(text);
+    }
+
+    /** Updates the opponent name on the UI. */
+    private void setOpponentName(String otherName) {
+        //otherText.setText(getString(R.string.opponent_name, otherName));
+    }
+
+    /** Enables/disables buttons depending on the connection status. */
+    private void setButtonState(boolean connected) {
+        connectButton.setEnabled(true);
+        connectButton.setVisibility(connected ? View.GONE : View.VISIBLE);
+        disconnectButton.setVisibility(connected ? View.VISIBLE : View.GONE);
+    }
 
 }
