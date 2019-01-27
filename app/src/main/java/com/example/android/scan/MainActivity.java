f58a7fc2f57880ff080d9f2b31a5b0929c9d093a
ns1017 <ns1017@ic.ac.uk>
2019-01-27 08:42:59 +0000

    manual merge with google_api

--- app/src/main/java/com/example/android/scan/MainActivity.java
+++ app/src/main/java/com/example/android/scan/MainActivity.java
@@ -1,18 +1,44 @@
 package com.example.android.scan;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.connection.Strategy;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -50,6 +76,108 @@ public class MainActivity extends AppCompatActivity {
     private String name;
     private File profile;
     private String[] possibleHobbies;
+
+    Payload payload_g;
+
+    private static final String TAG = "FREND.LY";
+
+    private static final String[] REQUIRED_PERMISSIONS =
+            new String[]{
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_WIFI_STATE,
+                    Manifest.permission.CHANGE_WIFI_STATE,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+            };
+
+    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
+
+
+
+    //Our Strategy
+    private static final Strategy STRATEGY = Strategy.P2P_STAR;
+
+    //Our handle to Nearby Clients
+    private ConnectionsClient connectionsClient;
+
+    private String otherID;
+    private String otherName;
+
+    private Button disconnectButton;
+    private Button connectButton;
+
+    private TextView statusText;
+    private TextView otherText;
+
+    // Callbacks for receiving payloads
+    private final PayloadCallback payloadCallback =
+            new PayloadCallback() {
+                @Override
+                public void onPayloadReceived(String endpointId, Payload payload) {
+                    payload_g = payload;
+                    File file = payload_g.asFile().asJavaFile();
+                    try {
+                        BufferedWriter writer = new BufferedWriter(new FileWriter("profile.txt"));
+                        writer.write(file.toString());
+                    } catch (IOException e) {
+                        System.out.println("File is empty\n"); }
+                }
+
+                @Override
+                public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
+                }
+            };
+
+    // Callbacks for finding other devices
+    private final EndpointDiscoveryCallback endpointDiscoveryCallback =
+            new EndpointDiscoveryCallback() {
+                @Override
+                public void onEndpointFound(String endpointId, DiscoveredEndpointInfo info) {
+                    Log.i(TAG, "onEndpointFound: endpoint found, connecting");
+                    connectionsClient.requestConnection(name, endpointId, connectionLifecycleCallback);
+                }
+
+                @Override
+                public void onEndpointLost(String endpointId) {}
+            };
+
+    // Callbacks for connections to other devices
+    private final ConnectionLifecycleCallback connectionLifecycleCallback =
+            new ConnectionLifecycleCallback() {
+                @Override
+                public void onConnectionInitiated(String endpointId, ConnectionInfo connectionInfo) {
+                    Log.i(TAG, "onConnectionInitiated: accepting connection");
+                    connectionsClient.acceptConnection(endpointId, payloadCallback);
+                    otherName = connectionInfo.getEndpointName();
+                }
+
+                @Override
+                public void onConnectionResult(String endpointId, ConnectionResolution result) {
+                    if (result.getStatus().isSuccess()) {
+                        Log.i(TAG, "onConnectionResult: connection successful");
+
+                        try {
+                            String currentDir = System.getProperty("user.dir");
+                            sendFile(new File(currentDir + "/test.txt"));
+                        } catch (FileNotFoundException e) {}
+
+                        connectionsClient.stopDiscovery();
+                        connectionsClient.stopAdvertising();
+
+                        otherID = endpointId;
+                        setOpponentName(otherName);
+                        //setStatusText(getString(R.string.status_connected));
+                        setButtonState(true);
+                    } else {
+                        Log.i(TAG, "onConnectionResult: connection failed");
+                    }
+                }
+
+                @Override
+                public void onDisconnected(String endpointId) {
+                    Log.i(TAG, "onDisconnected: disconnected from the opponent");
+                }
+            };
 
     private void readMyProfile() throws IOException {
         //mTestView.setText("readMyProfile");
@@ -135,6 +263,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+        disconnectButton = findViewById(R.id.disconnect);
+        connectButton = findViewById(R.id.connect);
+
+        //statusText = findViewById(R.id.status);
+
+        //TextView nameView = findViewById(R.id.name);
+        //nameView.setText(getString(R.string.codename, name));
+
+        connectionsClient = Nearby.getConnectionsClient(this);
+
+
         expListView = (ExpandableListView) findViewById(R.id.lvExp);
 
         prepareListData();
@@ -145,6 +284,111 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!hasPermissions(this, REQUIRED_PERMISSIONS)) {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        connectionsClient.stopAllEndpoints();
+        super.onStop();
+    }
+
+    /** Returns true if the app was granted all the permissions. Otherwise, returns false. */
+    private static boolean hasPermissions(Context context, String... permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /** Handles user acceptance (or denial) of our permission request. */
+    @CallSuper
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode != REQUEST_CODE_REQUIRED_PERMISSIONS) {
+            return;
+        }
+
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                //Toast.makeText(this, R.string.error_missing_permissions, Toast.LENGTH_LONG).show();
+                finish();
+                return;
+            }
+        }
+        recreate();
+    }
+
+
+    /** Finds an Frend using Nearby Connections. */
+    public void findFrend(View view) {
+        startAdvertising();
+        startDiscovery();
+        //setStatusText(getString(R.string.status_searching));
+    }
+
+    public void disconnect(View view) {
+        connectionsClient.disconnectFromEndpoint(otherID);
+    }
+
+
+    /** Starts looking for Frends using Nearby Connections. */
+    private void startDiscovery() {
+        // Note: Discovery may fail.
+        connectionsClient.startDiscovery(
+                getPackageName(), endpointDiscoveryCallback,
+                new DiscoveryOptions.Builder().setStrategy(STRATEGY).build());
+    }
+
+    /** Broadcasts our presence using Nearby Connections so other players can find us. */
+    private void startAdvertising() {
+        // Note: Advertising may fail.
+        connectionsClient.startAdvertising(
+                name, getPackageName(), connectionLifecycleCallback,
+                new AdvertisingOptions.Builder().setStrategy(STRATEGY).build());
+    }
+
+
+    /** Sends the user's file to the Frend. */
+    private void sendFile(File file) throws FileNotFoundException {
+
+        connectionsClient.sendPayload(
+                otherID, Payload.fromFile(file));
+
+        //setStatusText(getString(R.string.game_choice, choice.name()));
+        connectButton.setEnabled(false);
+    }
+
+    /** Shows a status message to the user. */
+    private void setStatusText(String text) {
+        statusText.setText(text);
+    }
+
+    /** Updates the opponent name on the UI. */
+    private void setOpponentName(String otherName) {
+        //otherText.setText(getString(R.string.opponent_name, otherName));
+    }
+
+    /** Enables/disables buttons depending on the connection status. */
+    private void setButtonState(boolean connected) {
+        connectButton.setEnabled(true);
+        connectButton.setVisibility(connected ? View.GONE : View.VISIBLE);
+        disconnectButton.setVisibility(connected ? View.VISIBLE : View.GONE);
     }
 
     public void gotoProfile() {
